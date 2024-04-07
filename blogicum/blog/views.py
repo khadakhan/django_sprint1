6a0310a7180c528ac7ef0d7def99e3be22325c07
Konstantin Khadakhanov <k-project@mail.ru>
2024-04-07 23:38:35 +0300

    Blogicum. Fifth iteration.

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -53,11 +53,10 @@ def index(request):
 
 
 def post_detail(request, post_id):
-    if post_id in posts_by_id:
-        return render(request, 'blog/detail.html',
-                      {'post': posts_by_id[post_id]})
-    else:
+    if post_id not in posts_by_id:
         raise Http404(f'Пост с id = {post_id} не найден в posts')
+    return render(request, 'blog/detail.html',
+                  {'post': posts_by_id[post_id]})
 
 
 def category_posts(request, category_slug):
